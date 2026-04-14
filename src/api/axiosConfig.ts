c1ecb8e1cc7853b3de7d8b3ae426a90d4373f0a0
m-ghozi <ghozi286@gmail.com>
2026-04-14 15:01:10 +0700

    🔐 feat: configure axios instance with environment variables and add .env to gitignore

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_KEY = import.meta.env.VITE_API_KEY;
+
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Prefix /api sesuai presensiRoutes
+    baseURL: BASE_URL,
     headers: {
         'Content-Type': 'application/json',
+        'X-API-KEY': API_KEY,
     },
 });
 
